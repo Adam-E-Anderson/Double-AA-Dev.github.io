12190515788f8990d5dbc88bd4b971e1584af0a8
Double-AA-Dev <adamea66@gmail.com>
2019-03-17 17:34:17 -0700

    First addition

---
+++ landing-page/index.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html>
+<head>
+    <title>Double AA Dev</title>
+	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <meta charset="utf-8" />
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <style type="text/css">
+		
+   body {
+        background-color: #c0dcf7;
+        margin: 0;
+        padding: 0;
+        font-family: 'Raleway', sans-serif;
+        
+    }
+	
+    #main {
+        width: 600px;
+        margin: 5em auto;
+        padding: 50px;
+        background-color: #9B9B9B;
+        border-radius: 1em;
+    }
+    a:link, a:visited {
+        color: #38488f;
+        text-decoration: none;
+    }
+	a:hover{
+		
+		text-decoration:underline;
+		
+	}
+    @media (max-width: 700px) {
+        body {
+            background-color: #fff;
+        }
+         .m-paragraph{
+            width: auto;
+            margin: 0 auto;
+            border-radius: 0;
+            padding: 1em;
+        }
+		
+		
+    }
+	#menu-bar{
+			
+			position: sticky;
+			top: 0px;
+			
+		}
+    </style>    
+</head>
+
+<body>
+
+	<div id="menu-bar">
+	
+		<p><a href="doubleaadev-com.stackstaging.com">Home</a><a href="https://github.com/Double-AA-Dev"> Github</a><a href="https://www.linkedin.com/in/adam-anderson-105a74b1/"> LinkedIn </a> <a href="https://twitter.com/DoubleAAdev"> Twitter</a></p>
+		<p><a href="hughes-creations.html">Hughes Creations</a></p>
+	
+	</div>
+<div id="main" class="m-paragraph">
+    <h1>Double AA Dev</h1>
+    <p>Hey All!</p>
+	<p>My name is Adam Anderson hence the Double A theme. I am on my coding journey and loving it! I am interested in web development and python scripting. I can't wait to update this site more with my journey as a code newbie. You can also find me on the <a href="https://twitter.com/DoubleAAdev">Twitter.com</a> Come back to see updates on the site as I learn and grow! -Double AA</p>
+</div>
+
+<div id="main" class="m-paragraph">
+    <h1>Five days in!</h1>
+    <p>Hey All!</p>
+	<p>So I am five days into the 100 days of code challenge! This challenge is awesome becuase it gives me the practice and drive to code everyday! I am learing some design aspects of the web. Like as you can see my menu bar looks less than appealing right now. I found a new font that I love! Raleway. Looks great. I just like how clean and simple it looks. Anyways make today great everyone! -Double AA</p>
+</div>
+
+</body>
+</html>
